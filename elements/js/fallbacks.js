95740ff87ad951c552490ba567591ae7470faaf2
Les James <ljames@capstrat.com>
2012-02-24 15:44:25 -0500

    added fallback script and sass mixins for cross browser

---
+++ elements/js/fallbacks.js
@@ -0,0 +1,7 @@
+(function(){
+	
+	if(!Modernizr.cssanimations) {
+		
+	}
+
+})(this, document);
--- sass/config.rb
+++ sass/config.rb
@@ -2,23 +2,17 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "../elements/css/"
-sass_dir = ""
-images_dir = "../elements/images/"
-javascripts_dir = "../elements/js/"
+css_dir = "/elements/css/"
+sass_dir = "/sass/"
+images_dir = "/elements/img/"
+javascripts_dir = "/elements/js/"
 
 # You can select your preferred output style here (can be overridden via the command line):
-output_style = :compressed
+output_style = :nested
+# output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
-
-
-# If you prefer the indented syntax, you might want to regenerate this
-# project again passing --syntax sass, or you can uncomment this:
-# preferred_syntax = :sass
-# and then run:
-# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
