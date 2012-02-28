778154e70ffb4009de0360e4c824f89f9e56a579
Les James <ljames@capstrat.com>
2012-02-28 16:55:27 -0500

    added ie classes to body tag, added no-svg mixin, fixed compass config

--- sass/config.rb
+++ sass/config.rb
@@ -2,13 +2,12 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "/elements/css/"
-sass_dir = "/sass/"
-images_dir = "/elements/img/"
-javascripts_dir = "/elements/js/"
+css_dir = "../elements/css/"
+sass_dir = ""
+images_dir = "../elements/img/"
+javascripts_dir = "../elements/js/"
 
 # You can select your preferred output style here (can be overridden via the command line):
-output_style = :nested
 # output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
