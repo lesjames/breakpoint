057c9aa4b34c37d8a11d49fdbccdf12d8858e58d
Les James <les@lesjames.com>
2012-10-09 16:17:59 -0400

    cleanup and comment js

--- elements/js/breakpoint.js
+++ elements/js/breakpoint.js
@@ -1,59 +1,63 @@
 (function($){
 
     $.fn.breakpoint = function(options) {
-    
+
         var resizeTimeout,
             that = this,
             currentLabel,
             settings = $.extend({
                 'delay' : 200,
                 'callback' : function(){},
-                'attrPrefix' : 'data-',
+                'prefix' : 'data-',
                 'fallback' : 'mobile'
             }, options),
             findLabel = function(){
                 // grab the current breakpoint label
                 var label = $('head').css('font-family');
-                return label;                
+                return label;
             },
-            searchArr = function(stringArray, label) {
-                for (var j=0; j<stringArray.length; j++) {
-                    var s = '^' + settings.attrPrefix + '' + label + '$';
-                    if (stringArray[j].match(s)) return true;
+            searchArr = function(arr, label) {
+                // does our source array have a match for our label?
+                for (var i=0; i<arr.length; i++) {
+                    var s = '^' + settings.prefix + label + '$';
+                    if (arr[i].match(s)) return true;
                 }
                 return false;
             },
             findSource = function(){
-                // looks at image and builds arr of available sources
-                var sourceArr = [];
+                // looks at image element and builds array of available sources
+                var arr = [];
                 for(var i = 0; i < this.attributes.length; i++) {
                     var s = this.attributes[i].nodeName;
-                    var r = '^' + settings.attrPrefix;
-                    if (s.match(r)) sourceArr.push(s);
+                    var r = '^' + settings.prefix;
+                    if (s.match(r)) arr.push(s);
                 }
-                return sourceArr;
+                return arr;
             },
             setSource = function(){
+                // what is our breakpoint label?
                 var label = findLabel();
-                
-                // should only fire if a change was made                
-                if (label !== currentLabel) {     
-                    that.each(function(){                    
-                        
-                        // find available data src
+
+                // should only fire if a change was made
+                if (label !== currentLabel) {
+                    that.each(function(){
+
+                        // does the element have a matching src for our label?
                         var srcArr = findSource.call(this),
                             arrMatch = searchArr(srcArr, label),
                             src;
-                        
-                        // if we don't have a matching src then use the fallback                      
-                        (arrMatch) ? src = $(this).attr(settings.attrPrefix + label) : src = $(this).attr(settings.attrPrefix + settings.fallback);
-                        
+
+                        // if we don't have a matching src then use the fallback
+                        (arrMatch) ? src = $(this).attr(settings.prefix + label) : src = $(this).attr(settings.prefix + settings.fallback);
+
                         // create a src for the image
                         $(this).attr('src', src);
-                        
+
                     });
                     // set current label
                     currentLabel = label;
+                    
+                    // if a callback was set then fire it once
                     settings.callback();
                 }
             };
@@ -63,9 +67,9 @@
             clearTimeout(resizeTimeout);
             resizeTimeout = setTimeout(setSource, settings.delay);
         });
-        
+
         // init
         return that.each(setSource);
-    
+
     }
 })(jQuery);
